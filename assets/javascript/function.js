3ab60e1831ee5859ea9d174b4d26fe32f6a7f881
William Gonzales <wgonz94@yahoo.com>
2019-10-04 23:05:31 -0700

    added js folder and file to add different levels of projects. (frontend & backend) Also, added liri toportfolio, need to add link for project

--- assets/javascript/function.js
+++ assets/javascript/function.js
@@ -1,9 +1,18 @@
 var frontend = $(".front-end")
+var backend = $(".back-end")
 
+backend.hide();
 frontend.hide();
 
 $(".front-button").on("click", frontEndProjects)
+$(".back-button").on("click", backEndProjects)
 
 function frontEndProjects() {
     frontend.show();
+    backend.hide();
+}
+
+function backEndProjects() {
+    frontend.hide();
+    backend.show();
 }
