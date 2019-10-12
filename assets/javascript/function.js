9280495734d1d39bd21d015773d7162061dde0b2
William Gonzales <wgonz94@yahoo.com>
2019-10-12 14:00:06 -0700

    input slideshow function with minor alterations

--- assets/javascript/function.js
+++ assets/javascript/function.js
@@ -10,9 +10,63 @@ $(".back-button").on("click", backEndProjects)
 function frontEndProjects() {
     frontend.show();
     backend.hide();
+    $(".front").hide();
+    $(".back").hide();
 }
 
 function backEndProjects() {
     frontend.hide();
     backend.show();
+    $("#image-holder").hide;
+    $("#image-holder2").hide;
 }
+
+function slideShowProj() {
+//Slideshow for project preivew
+var images = ["assets/images/Giftastic.JPG", "assets/images/musicalityLogo.JPG", "assets/images/TrainScheduler.JPG", "assets/images/psychicgame.JPG"];
+var count = 0;
+var showImage;
+
+function displayImage() {
+  $("#image-holder").html("<img src=" + images[count] + " width='400px' height='150'>");
+}
+function nextImage() {
+  count++;
+  $("#image-holder").html("<img src='assets/images/loading.gif' width='200px' height='150px'/>");
+  setTimeout(displayImage, 1000);
+  if (count === images.length) {
+    count = 0;
+  }
+}
+function startSlideshow() {
+  showImage = setInterval(nextImage, 3000);
+}
+displayImage();
+startSlideshow()
+}
+function slideShowBackend(){
+    //Slideshow for project preivew
+var images = ["assets/images/movie-this2.JPG", "assets/images/nodejs.png", "assets/images/express.jpg", "assets/images/comingsoon.png"];
+var count = 0;
+var showImage;
+
+function displayImage() {
+  $("#image-holder2").html("<img src=" + images[count] + " width='400px' height='150'>");
+}
+function nextImage() {
+  count++;
+  $("#image-holder2").html("<img src='assets/images/loading.gif' width='200px' height='150px'/>");
+  setTimeout(displayImage, 1000);
+  if (count === images.length) {
+    count = 0;
+  }
+}
+function startSlideshow() {
+  showImage = setInterval(nextImage, 3000);
+}
+displayImage();
+startSlideshow()
+}
+
+slideShowProj();
+slideShowBackend();
